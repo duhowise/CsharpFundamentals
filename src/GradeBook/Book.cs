b7f0bac95278b56b0ec008ba292e540857741a95
duho wise <wduho@hotmail.com>
2019-08-21 10:58:39 +0000

    Working with Classes and Objects

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,15 +1,43 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
     public class Book
     {
-        public Book () { }
+        List<double> grades;
+        string name;
 
+        public Book (string name)
+        {
+            this.name = name;
+            grades = new List<double> ();
+        }
         public void AddGrade (double grade)
         {
-            grades.Add(grade);
+            grades.Add (grade);
         }
-        List<double> grades;
+
+        public void ShowStats ()
+        {
+            var result = 0.0;
+
+            var totalNumberOfGrades = grades.Count;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                lowGrade = Math.Min (number, lowGrade);
+                highGrade = Math.Max (number, highGrade);
+                result += number;
+            }
+
+            System.Console.WriteLine ($"The lowest grade is {lowGrade}");
+            System.Console.WriteLine ($"The highest grade is {highGrade}");
+            System.Console.WriteLine ($"the sum of the grades are {result:N3}");
+            result /= totalNumberOfGrades;
+            System.Console.WriteLine ($"the average grade is {result:N3}");
+        }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,22 +9,18 @@ namespace GradeBook
 
         static void Main (string[] args)
         {
-            var book = new Book ();
+            var book = new Book ("Some Gradebook");
             book.AddGrade (12.3);
+            book.AddGrade (90.3);
+            book.AddGrade (98.3);
+            book.AddGrade (12.3);
+            book.AddGrade (10.8);
+            book.AddGrade (1.4);
+            book.AddGrade (56.1);
+            book.ShowStats ();
 
-            var grades = new List<double> { 12.3, 10.8, 1.4 };
-            grades.Add (56.1);
-            var result = 0.0;
-            var totalNumberOfGrades = grades.Count;
 
-            foreach (var grade in grades)
-            {
-                result += grade;
-            }
-
-            System.Console.WriteLine ($"the sum of the grades are {result:N3}");
-            result /= totalNumberOfGrades;
-            System.Console.WriteLine ($"the average grade is {result:N3}");
+            
 
         }
     }
